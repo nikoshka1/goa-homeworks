28230c1184c887d8274bee28467f0dbf571bfb50
nikoshka1 <mrnick12312@gmail.com>
2024-09-19 02:08:29 +0400

    'commit'

---
+++ day 4/classwork.py
@@ -0,0 +1,29 @@
+# Exercise 1:
+print("Hello, World!")
+
+# Exercise 2: 
+greeting = "Good Morning"
+print(greeting)
+
+# Exercise 3: 
+first_name = "Alice"
+last_name = "Smith"
+print("Name: ", first_name, last_name)
+
+# Exercise 4:
+number = 50
+print("The number is: ", number)
+
+# Exercise 5: 
+name = "George"
+print("Hello", name, "!")
+
+# Exercise 6: 
+age = 25
+print("I am", age, "years old.")
+
+# Exercise 7: 
+num1 = "10"
+num2 = "20"
+result = num1 + num2
+print(result)
